c64c839d6d2c922b3f3cbf4b1e72ce096593577a
Jiah Kwon <845872625@bun2-206-11.local>
2018-06-05 11:58:52 +1200

    working on border

---
+++ js/script.js
@@ -0,0 +1,90 @@
+
+	(function() {
+
+		//global var
+	  var store = []
+		var makeCard = document.getElementById('subbmit');
+		var images = document.getElementById('image');
+		var background = document.getElementById('background');
+		var font = document.getElementById('font');
+		var border = document.getElementById('border');
+    var cardImage = document.getElementById('cardImage');
+		var message = document.getElementById('message');
+		//making new style sheet to change ::After element in js
+		var styleElem = document.head.appendChild(document.createElement("style"));
+
+
+	//TEST
+	//when the button is clicked this fuction happens
+	$('#subbmit').click(function(){
+		 pictureChange();
+	}); // submit functuon ENDS
+
+
+	// change images of card with if / else statement
+	// if image's value is this, change the attribute
+ function pictureChange(){
+
+	 if (images.value == 'christmas'){
+		 cardImage.src = 'assets/christmas.jpg';
+	 } else if(images.value == 'birthday') {
+		 cardImage.src = 'assets/birthday.jpg';
+	 } else{
+		 cardImage.src = 'assets/valentines.jpg';
+	 }
+	 backgroundColor();
+	 fontChnage();
+	 borderChange();
+ }
+
+ //background color Chage functuon
+
+ function backgroundColor(){
+
+
+	 var back = document.getElementsByClassName('celadonBackground');
+	 if (background.value == 'celadon') {
+			styleElem.innerHTML = ".celadonBackground>div, .celadonBackground:after  {background: #9dcc78}"
+	 } else if(background.value == 'graphite') {
+		 styleElem.innerHTML = ".celadonBackground>div, .celadonBackground:after  {background: #aaa}"
+	 } else {
+		 styleElem.innerHTML = ".celadonBackground>div, .celadonBackground:after  {background: #F7C3EA}"
+
+	 }
+
+ }
+
+ //font chane function
+
+ function fontChnage() {
+	 var h1 = document.getElementsByClassName('sketch');
+	 if (font.value == 'handwriting') {
+		 	h1["0"].innerHTML = message.value
+			h1["0"].style.fontFamily = 'Pacifico'
+	 } else if (font.value == 'sketch') {
+		 h1["0"].innerHTML = message.value
+		 h1["0"].style.fontFamily = 'Fredericka the Great'
+
+	 }else{
+		 h1["0"].innerHTML = message.value
+		 h1["0"].style.fontFamily = 'Sigmar One'
+
+	 }
+
+ }
+
+ function borderChange (){
+	 var border = window.getComputedStyle(
+     document.querySelector('.noBorder'), ':after'
+ );
+
+ console.log(border);
+	 // if (border.value == 'thin') {
+		//  styleElem.style.border = "3em solid black"
+
+
+
+ }
+
+
+	}()); //iife ENDS
